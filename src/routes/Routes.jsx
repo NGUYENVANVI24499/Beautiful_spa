10985c6ec4fb1c9513583075aa3943cfa128b7e9
Nguyễn Văn Vĩ <43158136+NGUYENVANVI24499@users.noreply.github.com>
2022-05-31 06:23:22 +0700

    service update

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -9,9 +9,10 @@ import {
 import Home from '../pages/Home'
 import Introduce from '../pages/Introduce'
 import Product from '../pages/Product'
-import Service from '../pages/Service'
+import Category_Service from '../pages/Category_Service'
 import Treatment_results from '../pages/Treatment_results'
 import Recruitment from '../pages/Recruitment';
+import Serviceis from '../conponents/Service/Serviceis';
 
 const Routess = () => {
   return (
@@ -19,7 +20,8 @@ const Routess = () => {
        <Route path= '/' exact  element= {<Home />} />
        <Route path= '/catalog/:slug'  element= {<Product />} />
        <Route path= '/Introduce'  element= {<Introduce />} />
-       <Route path= '/Service'  element= {<Service />} />
+       <Route path= '/Service/:slug'  element= {<Serviceis />} />
+       <Route path='/Service' element ={<Category_Service />} />
        <Route path='/Treatment_results' element = {<Treatment_results />} />
        <Route path='/tuyen-dung' element = {<Recruitment />} />
     </Routes>
